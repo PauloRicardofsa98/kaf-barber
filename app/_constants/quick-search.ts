4745a288744e0f6ce673dceea89f374d7cc0962b
Paulo Ricardo <paulo.ricardofsa98@gmail.com>
2025-02-17 21:07:17 -0300

    feat: add BookingItem component

---
+++ app/_constants/quick-search.ts
@@ -0,0 +1,31 @@
+type QuickSearchOptions = {
+  imageUrl: string;
+  title: string;
+};
+
+export const quickSearchOptions: QuickSearchOptions[] = [
+  {
+    imageUrl: "/cabelo.svg",
+    title: "Cabelo",
+  },
+  {
+    imageUrl: "/barba.svg",
+    title: "Barba",
+  },
+  {
+    imageUrl: "/barba.svg",
+    title: "Acabamento",
+  },
+  {
+    imageUrl: "/sobrancelha.svg",
+    title: "Sobrancelha",
+  },
+  {
+    imageUrl: "/massagem.svg",
+    title: "Massagem",
+  },
+  {
+    imageUrl: "/hidratacao.svg",
+    title: "Hidratação",
+  },
+];
--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ import { Button } from "@/app/_components/ui/button";
 import { Input } from "@/app/_components/ui/input";
 
 import BarbershopItem from "./_components/barbershop-item";
-import { Avatar, AvatarImage } from "./_components/ui/avatar";
-import { Badge } from "./_components/ui/badge";
+import BookingItem from "./_components/booking-item";
 import { Card, CardContent } from "./_components/ui/card";
+import { quickSearchOptions } from "./_constants/quick-search";
 import { db } from "./_lib/prisma";
 
 const HomePage = async () => {
@@ -34,40 +34,17 @@ const HomePage = async () => {
         </div>
 
         <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
-          <Button className="gap-2" variant="secondary">
-            <Image src="/cabelo.svg" alt="Cabelo" width={16} height={16} />
-            Cabelo
-          </Button>
-          <Button className="gap-2" variant="secondary">
-            <Image src="/barba.svg" alt="Barba" width={16} height={16} />
-            Barba
-          </Button>
-          <Button className="gap-2" variant="secondary">
-            <Image src="/barba.svg" alt="Acabamento" width={16} height={16} />
-            Acabamento
-          </Button>
-          <Button className="gap-2" variant="secondary">
-            <Image
-              src="/sobrancelha.svg"
-              alt="Sobrancelha"
-              width={16}
-              height={16}
-            />
-            Sobrancelha
-          </Button>
-          <Button className="gap-2" variant="secondary">
-            <Image src="/massagem.svg" alt="Massagem" width={16} height={16} />
-            Massagem
-          </Button>
-          <Button className="gap-2" variant="secondary">
-            <Image
-              src="/hidratacao.svg"
-              alt="Hidratação"
-              width={16}
-              height={16}
-            />
-            Hidratação
-          </Button>
+          {quickSearchOptions.map((option) => (
+            <Button key={option.title} variant="secondary">
+              <Image
+                src={option.imageUrl}
+                alt={option.title}
+                width={16}
+                height={16}
+              />
+              {option.title}
+            </Button>
+          ))}
         </div>
 
         <div className="relative mt-6 h-[150px] w-full">
@@ -79,30 +56,7 @@ const HomePage = async () => {
           />
         </div>
 
-        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-          Agendamentos
-        </h2>
-        <Card>
-          <CardContent className="flex justify-between">
-            <div className="flex flex-col gap-2 py-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de cabelo</h3>
-
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
-                </Avatar>
-                <p className="text-sm">Barbearia Kaf</p>
-              </div>
-            </div>
-
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Agosto</p>
-              <p className="text-2xl">04</p>
-              <p className="text-sm">20:00</p>
-            </div>
-          </CardContent>
-        </Card>
+        <BookingItem />
 
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
